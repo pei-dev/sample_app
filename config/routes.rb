a943f814707546e47b18fb997d857a63c4a84b00
pei <shhxshh.1010@gmail.com>
2018-10-14 23:37:36 +0900

    Finish static pages (chp3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_page#home'
   get 'static_page/home'
   get 'static_page/help'
   get 'static_page/about'
@@ -6,7 +7,6 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 
 
 end
