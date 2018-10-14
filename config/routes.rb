5e794d68a7596c54027ef79152ecb5552e97a9b1
pei <shhxshh.1010@gmail.com>
2018-10-14 23:18:25 +0900

    chp3 just before 3.4.4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'static_page/home'
   get 'static_page/help'
   get 'static_page/about'
-  
+  get 'static_page/contact'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
